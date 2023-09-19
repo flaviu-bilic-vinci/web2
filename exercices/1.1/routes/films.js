b8e46b1ddb9f5b5e72a9f1faec5686b7bb1886b3
Bilic <flaviu.bilic@student.vinci.be>
2023-09-19 15:24:36 +0200

    first commit

---
+++ exercices/1.1/routes/films.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+let film1 = {
+    id: 1,
+    title: "placeholderTitle1",
+    duration: 120,
+    budget: 20,
+    link: "https://www.imdb.com/title/tt13026902/" 
+}
+
+let film2 = {
+    id: 2,
+    title: "placeholderTitle2",
+    duration: 120,
+    budget: 40,
+    link: "https://www.imdb.com/title/tt13026902/" 
+}
+
+let film3 = {
+    id: 3,
+    title: "placeholderTitle3",
+    duration: 140,
+    budget: 10,
+    link: "https://www.imdb.com/title/tt13026902/" 
+}
+
+const MOVIES = [];
+MOVIES.push(film1,film2,film3);
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
