8724131e6eecc29762ef8922dfde9b72412f764b
Bilic <flaviu.bilic@student.vinci.be>
2023-09-25 11:18:04 +0200

    idktrythis

--- exercices/1.1/routes/films.js
+++ exercices/1.1/routes/films.js
@@ -31,8 +31,7 @@ MOVIES.push(film1,film2,film3);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    
-  res.json(MOVIES);
+    res.json(MOVIES);
 });
 
 module.exports = router;
