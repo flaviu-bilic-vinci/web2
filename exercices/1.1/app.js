e16cd0388264f020bac2b947cd234bd409e40969
Bilic <flaviu.bilic@student.vinci.be>
2023-09-25 11:27:19 +0200

    exo 1.2 creation

--- exercices/1.1/app.js
+++ exercices/1.1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/films');
+var filmsRouter = require('./routes/films');
 
 var app = express();
 
@@ -13,10 +13,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/films', usersRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
